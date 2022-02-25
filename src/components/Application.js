0d94e8124c88482cad077440ad36e54dd756b51b
Thomas Ngo <thomasngo818@gmail.com>
2022-02-25 09:35:13 -0800

    Update useEffect to set the state to data from get request

--- src/components/Application.js
+++ src/components/Application.js
@@ -64,7 +64,8 @@ export default function Application(props) {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers")
     ]).then((all) => {
-      console.log(all);
+      // console.log(all);
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
     });
   }, []);
 
