046ec335217ad3e4fb91b13a8df15c411e1bece5
Thomas Ngo <thomasngo818@gmail.com>
2022-02-28 09:27:37 -0800

    Add selector function to get interviewer based on day as an array

--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -14,6 +14,22 @@ export function getAppointmentsForDay(state, day) {
   return appointments;
 }
 
+export function getInterviewersForDay(state, day) {
+  let interviewers = [];
+
+  const appointmentDay = state.days.find(appointmentDay => {
+    return appointmentDay.name === day
+  })
+
+  if (appointmentDay) {
+    interviewers = appointmentDay.interviewers.map((id) => {
+      return state.interviewers[id];
+    });
+  }
+
+  return interviewers;
+}
+
 export function getInterview(state, interview) {
   let interviewDetails = null;
 
