f206e0c5a380b37dbc4caa47de7d2b6e21910ecf
Thomas Ngo <thomasngo818@gmail.com>
2022-03-01 08:46:36 -0800

    Add initial api call to get state data

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData(initial) {
   const [state, setState] = useState(initial);
 
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    });
+  }, []);
+  
   const setDay = day => setState({...state, day});
 
   function bookInterview(id, interview) {
