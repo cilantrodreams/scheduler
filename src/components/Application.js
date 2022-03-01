c5d738abe0fe7371a3272aeb6649c50411845a37
Thomas Ngo <thomasngo818@gmail.com>
2022-02-28 21:22:50 -0800

    Update bookInterview to make put request before setting the state

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,7 +31,6 @@ export default function Application(props) {
   }); 
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -40,7 +39,11 @@ export default function Application(props) {
       ...state.appointments,
       [id]: appointment
     };
-    setState({...state, appointments});
+
+    return axios.put(`/api/appointments/${id}`, {interview})
+      .then(() => {
+        setState({...state, appointments});
+      });
   }
 
   useEffect(() => {
