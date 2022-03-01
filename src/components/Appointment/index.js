3105203d4cc4b0b9d6929239f79718f3d9107609
Thomas Ngo <thomasngo818@gmail.com>
2022-02-28 22:04:23 -0800

    Import Confirm component and add conditional logic to show it if mode is set to CONFIRM

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -14,7 +15,7 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
-
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   const  { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
@@ -55,6 +56,7 @@ export default function Appointment(props) {
         />
       )}
       {mode === SAVING && <Status />}
+      {mode === CONFIRM && <Confirm />}
     </article>
   );
 };
