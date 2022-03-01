76fc95b9d14f3cd2e5dc32dfb5b664f2c70631e6
Thomas Ngo <thomasngo818@gmail.com>
2022-03-01 08:40:00 -0800

    Moved application state and all functions to manage it here to be exported as a hook

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,41 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default function useApplicationData(initial) {
   const [state, setState] = useState(initial);
 
+  const setDay = day => setState({...state, day});
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.put(`/api/appointments/${id}`, {interview})
+      .then(() => {
+        setState({...state, appointments});
+      });
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    }
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+    return axios.delete(`/api/appointments/${id}`)
+      .then(() => {
+        setState({...state, appointments});
+      });
+  }
+
+  return { state, setDay, bookInterview, cancelInterview}
 }
