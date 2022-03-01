ae472150b2987cdd70c7eacd67f13af8d4dec024
Thomas Ngo <thomasngo818@gmail.com>
2022-03-01 08:25:08 -0800

    Pass back function to error mode components as props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -84,11 +84,13 @@ export default function Appointment(props) {
       {mode === ERROR_SAVE && (
         <Error
           message={"Could not make appointment"}
+          onClose={back}
         />
       )}
       {mode === ERROR_DELETE && (
         <Error
           message={"Could not cancel appointment"}
+          onClose={back}
         />
       )}
     </article>
