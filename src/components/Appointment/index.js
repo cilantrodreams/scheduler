a30353fed47aa7fd4a8910b6a05ff1635f5cf825
Thomas Ngo <thomasngo818@gmail.com>
2022-02-28 21:24:55 -0800

    Update save function to transition to Show in the callback of the promise returned by bookInterview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -24,7 +24,7 @@ export default function Appointment(props) {
       interviewer
     };
     props.bookInterview(props.id, interview)
-    transition(SHOW);
+      .then(() => transition(SHOW))
   }
 
   return (
